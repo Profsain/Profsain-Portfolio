a8f8306acc31e1f92433f89681d83fd754fabe39
Profsain <profsaincrown@gmail.com>
2022-05-10 14:22:23 +0100

    Fix brand logo visibility issues

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,15 +1,18 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamburgar = document.getElementById('hamburger-icon');
 const navClose = document.querySelectorAll('.mobile-nav-link');
+const brand = document.querySelector('#brand');
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('open');
   hamburgar.classList.toggle('change');
+  brand.classList.toggle('closeBrand');
 }
 
 function closeNav() {
   hamburgar.classList.remove('change');
   mobileMenu.classList.remove('open');
+  brand.classList.remove('closeBrand');
 }
 
 hamburgar.addEventListener('click', toggleMobileMenu);
