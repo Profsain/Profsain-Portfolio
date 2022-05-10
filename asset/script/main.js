0e12d37f634b2e505a5bef909cb57b2d76c2e2fa
Henry <dokinruss@gmail.com>
2022-05-10 10:18:51 +0000

    Add external javascript to close navbar when link is click

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,5 +1,14 @@
 let mobileMobile = document.getElementsByClassName("mobile-menu");
 let hamburgar = document.getElementById("hamburger-icon");
+let closeNavBar = document.querySelectorAll(".nav-link");
+
+
+
+for(i = 0; i < closeNavBar.length; i++ ){
+    closeNavBar[i].addEventListener("click", closeNav );
+}
+
+
 function toggleMobileMenu(bars) {
     bars.classList.toggle("change");
     mobileMobile[0].classList.toggle("open");
