d568b8dadea355545c49d6115cef30758c83690b
Profsain <profsaincrown@gmail.com>
2022-05-11 23:57:42 +0100

    fix linters error

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,4 +1,6 @@
 const projectDatabase = database;
+const mainPage = document.querySelector('.main-page');
+const modalBox = document.querySelector('.modal-container');
 const portfolioGrid = document.querySelector('.portfolio-grid');
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamburgar = document.getElementById('hamburger-icon');
@@ -9,9 +11,47 @@ const seeSource = document.querySelector('#see-source');
 let seeLiveUrl = '';
 let seeSourceUrl = '';
 
+// modal box details builder
+const buildModalBox = (projectId) => {
+  let modalObj = {};
+
+  //filter object
+  for (let i = 0; i < projectDatabase.length; i += 1) {
+    if (projectDatabase[i].id === projectId) {
+      modalObj = {...projectDatabase[i]};
+    };
+  }
+
+  seeLiveUrl = modalObj.liveVersionUrl;
+  seeSourceUrl = modalObj.sourceCodeUrl;
+
+  const modalTechnologies = document.querySelector('.modal-technologies');
+  const modalImage = document.querySelector('.modal-image');
+  const modalTitle = document.querySelector('.modal-title');
+  const modalDetail = document.querySelector('.modal-details-text');
+  const technologiesItems = document.createElement('ul');
+  technologiesItems.classList.add('card-tags');  
+  modalImage.setAttribute('src', modalObj.featuredImage);
+  modalTitle.innerText = modalObj.projectName;
+  modalDetail.innerText = modalObj.projectDescription;
+
+  modalObj.technologies.forEach((elem) => {
+    const item = document.createElement('li');
+    item.classList.add('tag-text');
+    item.innerText = elem;
+    technologiesItems.append(item);
+  });
+
+  modalTechnologies.replaceChildren(technologiesItems);
+}
+
+window.onload = () => {
+  buildProjectCard();
+};
+
 // see project handler
 const seeProjectBtnHandler = (event) => {
-  let id = parseInt(event.target.id);
+  const id = parseInt(event.target.id, 10);
   buildModalBox(id);
   mainPage.style.display = 'none';
   modalBox.style.display = 'block';
@@ -43,62 +83,18 @@ const buildProjectCard = () => {
       techItem.innerText = item;
       technologies.append(techItem);
     });
-
     sectionCard.append(featuredImg, projectTitle, technologies, projectBtn);
     portfolioGrid.append(sectionCard);
   });
- 
 };
-
-// open modal box
-const mainPage = document.querySelector('.main-page');
-const modalBox = document.querySelector('.modal-container');
 
 // close modal box when user click anywhere outside the modal
 window.onclick = (event) => {
-  if (event.target == modalBox) {
+  if (event.target === modalBox) {
     modalBox.style.display = 'none';
     mainPage.style.display = 'block';
-  }
+  };
 }
-
-// modal box details builder
-const buildModalBox = (projectId) => {
-  let modalObj = {};
-
-  //filter object
-  for (let i = 0; i < projectDatabase.length; i++) {
-    if (projectDatabase[i].id === projectId) {
-      modalObj = {...projectDatabase[i]};
-    }
-  }
-
-  seeLiveUrl = modalObj.liveVersionUrl;
-  seeSourceUrl = modalObj.sourceCodeUrl;
-
-  const modalTechnologies = document.querySelector('.modal-technologies');
-  const modalImage = document.querySelector('.modal-image');
-  const modalTitle = document.querySelector('.modal-title');
-  const modalDetail = document.querySelector('.modal-details-text');
-  const technologiesItems = document.createElement('ul');
-  technologiesItems.classList.add('card-tags');  
-  modalImage.setAttribute('src', modalObj.featuredImage);
-  modalTitle.innerText = modalObj.projectName;
-  modalDetail.innerText = modalObj.projectDescription;
-
-  modalObj.technologies.forEach((elem) => {
-    const item = document.createElement('li');
-    item.classList.add('tag-text');
-    item.innerText = elem;
-    technologiesItems.append(item);
-  });
-
-  modalTechnologies.replaceChildren(technologiesItems);
-}
-
-window.onload = () => {
-  buildProjectCard();
-};
 
 // close modal box when user click X button
 const closeBoxModel = () => {
@@ -128,13 +124,13 @@ const toggleMobileMenu = () => {
   mobileMenu.classList.toggle('open');
   hamburgar.classList.toggle('change');
   brand.classList.toggle('closeBrand');
-}
+};
 
 const closeNav = () => {
   hamburgar.classList.remove('change');
   mobileMenu.classList.remove('open');
   brand.classList.remove('closeBrand');
-}
+};
 
 hamburgar.addEventListener('click', toggleMobileMenu);
 
