50ded4e70154bc94ed34d5ac59284dc21fa9859a
profsain <husseinimudiking@gmail.com>
2023-04-20 09:41:52 +0100

    update links

--- asset/script/database.js
+++ asset/script/database.js
@@ -24,7 +24,7 @@ const database = [
     projectDescription: 'Bookstore CMS is a React application that allow user to add books their are currently reading to stores, display book list, update current chapter being read, add comment, monitor reading progress and also arrange books into various category. The project is built with React, Redux, and API.',
     featuredImage: 'https://i.postimg.cc/cC2r7G0X/bookstorecms.jpg',
     technologies: ['React', 'Redux', 'CSS', 'JavaScript', 'HTML', 'Microverse API'],
-    liveVersionUrl: 'https://bookstore-react.herokuapp.com/',
+    liveVersionUrl: 'https://book-store-cms-x2nl.onrender.com/',
     sourceCodeUrl: 'https://github.com/Profsain/Bookstore-CMS',
   },
   {
@@ -33,7 +33,7 @@ const database = [
     projectDescription: 'A react-redux web app that helps users to search for weather conditions of any city, view location & date, view wind status & wind direction, view humidity, and air pressure, view weather conditions of the next 5 days, and convert from degrees Celsius to Fahrenheit or vice visa',
     featuredImage: 'https://i.postimg.cc/c4WfKsgJ/weatherapp.jpg',
     technologies: ['React', 'Redux', 'CSS', 'JavaScript', 'HTML'],
-    liveVersionUrl: 'https://joyful-arithmetic-e662f9.netlify.app/weather',
+    liveVersionUrl: 'https://city-weather-app.onrender.com/',
     sourceCodeUrl: 'https://github.com/Profsain/city-weather',
   },
   {
@@ -51,7 +51,7 @@ const database = [
     projectDescription: 'A React-Redux web application for a commercial and scientific space travel services company. Rockets section displays a list of all available SpaceX rockets. Users can book each rocket by clicking the reservation button or cancel the previously made booking. The Missions section displays a list of current missions along with their brief description. Users can join any of the missions by clicking the join mission button or leave the mission by clicking the leave mission button. The My Profile section displays all reserved rockets and joined missions. Users can cancel the reservation or leave the mission by clicking the respective buttons',
     featuredImage: 'https://i.postimg.cc/cJTFhXjV/spacexapp.jpg',
     technologies: ['React', 'Redux', 'CSS', 'JavaScript', 'HTML'],
-    liveVersionUrl: 'https://tourmaline-cheesecake-5ae994.netlify.app/',
+    liveVersionUrl: 'https://space-app-ht7b.onrender.com/',
     sourceCodeUrl: 'https://github.com/Profsain/space-travel-hub',
   },
 ];
